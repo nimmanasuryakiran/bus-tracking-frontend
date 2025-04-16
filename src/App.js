325e4fb83ff385352e908afbd27f9e7b97d63bc4
nimmanasuryakiran <suryakirannimmana@gmail.com>
2025-04-16 16:35:39 +0530

    commit for 7 update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-
-
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import 'leaflet/dist/leaflet.css';
-import "./styles.css";
+import StudentMap from "./components/StudentMap";
 
-import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DriverLogin from "./components/DriverLogin";
-import StudentLogin from "./components/StudentLogin";
 import DriverTracker from "./components/DriverTracker";
 import LeafletMapComponent from "./components/LeafletMapComponent";
-import StudentMap from "./components/StudentMap";
+import 'leaflet/dist/leaflet.css';
+import "./styles.css";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<DriverLogin />} />
                 <Route path="/driver-login" element={<DriverLogin />} />
-                <Route path="/student-login" element={<StudentLogin />} />
                 <Route path="/driver-tracker" element={
                     <>
                         <DriverTracker busId={"BUS-101"} />
@@ -26,9 +21,14 @@ function App() {
                     </>
                 } />
                 <Route path="/student-map" element={<StudentMap />} />
+                
             </Routes>
+
         </Router>
+        
     );
 }
 
 export default App;
+
+
